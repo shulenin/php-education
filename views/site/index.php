b91efc8f812d7f07b3c8a0e397a8270ff6f35c80
shulenin <dima.shulenin@mail.ru>
2022-07-20 18:24:27 +0300

    test

--- views/site/index.php
+++ views/site/index.php
@@ -18,7 +18,7 @@ $this->params['breadcrumbs'][] = $this->title;
     <h1><?= Html::encode($this->title) ?></h1>
 
     <p>
-        <?= Html::a('Создать страну', ['create'], ['class' => 'btn btn-success']) ?>
+        <?= Html::a('Create Country', ['create'], ['class' => 'btn btn-success']) ?>
     </p>
 
     <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
